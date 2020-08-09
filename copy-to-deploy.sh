d35155a984fcb9d28001917019d323ac799fcdfa
Ben Hussey <ben@blip2.net>
2020-08-09 18:23:40 +1000

    deploy to live

---
+++ copy-to-deploy.sh
@@ -0,0 +1,4 @@
+scp -r wwwroot/ ben@greenprints.blip2.net:~/GreenprintsMap/
+scp -r node_modules/ ben@greenprints.blip2.net:~/GreenprintsMap/
+scp -r devserverconfig.json ben@greenprints.blip2.net:~/GreenprintsMap/
+scp -r runserver.sh ben@greenprints.blip2.net:~/GreenprintsMap/ 
